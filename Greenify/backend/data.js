e0851ea8606fd960cc466e1ab7603ab90b3c46ef
LeshanieM <Leshaniebiz@gmail.com>
2025-01-31 17:26:30 +0530

    Improve Add To Cart

--- Greenify/backend/data.js
+++ Greenify/backend/data.js
@@ -1,6 +1,7 @@
 const data = {
   products: [
     {
+      _id: '1',
       name: 'Rose Plant',
       slug: 'rose-plant',
       category: 'Plants',
@@ -14,6 +15,7 @@ const data = {
     },
 
     {
+      _id: '2',
       name: 'Banyan Plant',
       slug: 'banyan-plant',
       category: 'Plants',
@@ -27,6 +29,7 @@ const data = {
     },
 
     {
+      _id: '3',
       name: 'Mango Plant',
       slug: 'mango-plant',
       category: 'Plants',
@@ -40,6 +43,7 @@ const data = {
     },
 
     {
+      _id: '4',
       name: 'Orchid Plant',
       slug: 'orchid-plant',
       category: 'Flowers',
