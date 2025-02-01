f9429772fac055baa3e8755d17274d11bad4966b
LeshanieM <Leshaniebiz@gmail.com>
2025-02-02 01:11:42 +0530

    Completed Cart Screen

--- Greenify/frontend/src/Store.js
+++ Greenify/frontend/src/Store.js
@@ -4,30 +4,40 @@ export const Store = createContext();
 
 const initialState = {
   cart: {
-    cartItems: [],
+    cartItems: localStorage.getItem('cartItems') // cart is stored in browser
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
   },
 };
 
 function reducer(state, action) {
-    switch (action.type) {
-      case 'CART_ADD_ITEM':
-        // add to cart
+  switch (action.type) {
+    case 'CART_ADD_ITEM':
+      // add to cart
 
-        const newItem = action.payload;
-        const existItem = state.cart.cartItems.find(
-          (item) => item._id === newItem._id    
-        );
-        const cartItems = existItem
+      const newItem = action.payload;
+      const existItem = state.cart.cartItems.find(
+        (item) => item._id === newItem._id
+      );
+      const cartItems = existItem
         ? state.cart.cartItems.map((item) =>
             item._id === existItem._id ? newItem : item
           )
         : [...state.cart.cartItems, newItem];
-        return { ...state, cart: { ...state.cart, cartItems } };
-      
-        
-      default:
-        return state;
+      localStorage.setItem('cartItems', JSON.stringify(cartItems)); // cart is stored in browser
+      return { ...state, cart: { ...state.cart, cartItems } };
+
+    case 'CART_REMOVE_ITEM': {
+      const cartItems = state.cart.cartItems.filter(
+        (item) => item._id !== action.payload._id
+      );
+      localStorage.setItem('cartItems', JSON.stringify(cartItems)); // cart is stored in browser
+      return { ...state, cart: { ...state.cart, cartItems } };
     }
+
+    default:
+      return state;
+  }
 }
 
 export function StoreProvider(props) {
--- Greenify/frontend/src/components/Product.js
+++ Greenify/frontend/src/components/Product.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Rating from './Rating';
+import { Store } from '../Store';
+import axios from 'axios';
 
 function Product(props) {
   const { product } = props;
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  const addToCartHandler = async (item) => {
+    const existItem = cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...item, quantity },
+    });
+  };
+
   return (
     <Card>
       <Link to={`/product/${product.slug}`}>
@@ -19,7 +41,14 @@ function Product(props) {
         <Rating rating={product.rating} numReviews={product.numReviews} />
 
         <Card.Text>${product.price}</Card.Text>
-        <Button>Add to Cart</Button>
+
+        {product.countInStock === 0 ? (
+          <Button variant="light" disabled>
+            Out of stock
+          </Button>
+        ) : (
+          <Button onClick={() => addToCartHandler(product)}>Add to Cart</Button>
+        )}
       </Card.Body>
     </Card>
   );
