920fcc49db5d8f970ace2714ce72a47edca0ff5b
LeshanieM <Leshaniebiz@gmail.com>
2025-02-03 18:27:24 +0530

    21- Create Sample Users

--- Greenify/backend/routes/seedRoutes.js
+++ Greenify/backend/routes/seedRoutes.js
@@ -2,13 +2,18 @@ import express from 'express';
 import data from '../data.js';
 import Product from '../models/productModel.js';
 import e from 'express';
+import User from '../models/userModel.js';
 
 const seedRouter = express.Router();
 
 seedRouter.get('/', async (req, res) => {
-  await Product.deleteMany({});//remove changed as deleteMany - new version
+  await Product.deleteMany({}); //remove changed as deleteMany - new version
   const createdProducts = await Product.insertMany(data.products);
-  res.send({ createdProducts });
+  // res.send({ createdProducts });
+
+  await User.deleteMany({}); //remove changed as deleteMany - new version
+  const createdUsers = await User.insertMany(data.users);
+  res.send({ createdProducts, createdUsers });
 });
 
 export default seedRouter;
