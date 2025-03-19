4c407f3499852642d52332980b4863d7a4feed46
LeshanieM <Leshaniebiz@gmail.com>
2025-03-19 10:48:02 +0530

    some code changed

--- Lavendra/backend/routes/seedRoutes.js
+++ Lavendra/backend/routes/seedRoutes.js
@@ -5,15 +5,19 @@ import e from 'express';
 import User from '../models/userModel.js';
 
 const seedRouter = express.Router();
+try {
+  seedRouter.get('/', async (req, res) => {
+    await Product.deleteMany({}); //remove changed as deleteMany - new version
+    const createdProducts = await Product.insertMany(data.products);
+    // res.send({ createdProducts });
 
-seedRouter.get('/', async (req, res) => {
-  await Product.deleteMany({}); //remove changed as deleteMany - new version
-  const createdProducts = await Product.insertMany(data.products);
-  // res.send({ createdProducts });
-
-  await User.deleteMany({}); //remove changed as deleteMany - new version
-  const createdUsers = await User.insertMany(data.users);
-  res.send({ createdProducts, createdUsers });
-});
+    await User.deleteMany({}); //remove changed as deleteMany - new version
+    const createdUsers = await User.insertMany(data.users);
+    res.send({ createdProducts, createdUsers });
+  });
+} catch (error) {
+  console.error('Error seeding data:', error);
+  res.status(500).send({ message: 'Error seeding data' });
+}
 
 export default seedRouter;
