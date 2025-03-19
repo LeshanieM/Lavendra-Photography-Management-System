4f90c028bca172e83c13d7e1f69e32ab973850d5
LeshanieM <Leshaniebiz@gmail.com>
2025-03-19 12:16:16 +0530

    Create Sign-Up Screen

--- Lavendra/frontend/src/App.js
+++ Lavendra/frontend/src/App.js
@@ -16,7 +16,8 @@ import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
-import AddressScreen from './screens/AddressScreen';
+import AddressScreen from './screens/PhotographyVenueScreen';
+import SignupScreen from './screens/SignupScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -82,6 +83,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
               <Route path="/shipping" element={<AddressScreen />} />
               <Route
                 path="/create-package"
